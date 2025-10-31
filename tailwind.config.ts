ba38a1f5e264c5a043b40636417322472efdd8fc
Nitin Chorge <chorgenitin105@gmail.com>
2025-10-31 18:17:22 +0000

    You have not added tailwind css may be thats why classes are not working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,6 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  safelist: [
-    {
-      pattern: /rounded-\[\d+px\]/,
-    }
-  ],
   theme: {
     extend: {
       fontFamily: {
