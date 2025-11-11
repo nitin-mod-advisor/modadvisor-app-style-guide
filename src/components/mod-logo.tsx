5b866e99b8205789fd7eb3cebfc10e07110479f9
Nitin Chorge <chorgenitin105@gmail.com>
2025-11-11 12:35:48 +0000

    Again you used svg use images from public folder

--- src/components/mod-logo.tsx
+++ src/components/mod-logo.tsx
@@ -1,58 +1,28 @@
-"use client"
 
-import { cn } from "@/lib/utils";
+'use client';
+
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
 
 export function ModLogo({ className }: { className?: string }) {
   return (
     <div className={cn("relative w-8 h-8", className)}>
       {/* Black logo for light mode */}
-      <svg
+      <Image
+        src="/logo-black.png"
+        alt="Logo"
+        width={32}
+        height={32}
         className="dark:hidden"
-        width="32"
-        height="32"
-        viewBox="0 0 48 48"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M24 4L4 24L24 44L44 24L24 4Z"
-          fill="black"
-          stroke="black"
-          strokeWidth="4"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M34 24L24 34L14 24L24 14L34 24Z"
-          fill="black"
-          stroke="white"
-          strokeWidth="4"
-          strokeLinejoin="round"
-        />
-      </svg>
+      />
       {/* White logo for dark mode */}
-      <svg
+      <Image
+        src="/logo-white.png"
+        alt="Logo"
+        width={32}
+        height={32}
         className="hidden dark:block"
-        width="32"
-        height="32"
-        viewBox="0 0 48 48"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M24 4L4 24L24 44L44 24L24 4Z"
-          fill="white"
-          stroke="white"
-          strokeWidth="4"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M34 24L24 34L14 24L24 14L34 24Z"
-          fill="white"
-          stroke="black"
-          strokeWidth="4"
-          strokeLinejoin="round"
-        />
-      </svg>
+      />
     </div>
   );
 }
